b02fe3265693e87fe7ae75b33633fff3c6cc7502
wahaz <rizaldi.wahaz@gmail.com>
2019-12-23 10:30:38 +0700

    initial commit

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,52 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+
+	public function qr()
+	{
+		$npm = $this->db->get('npm')->result();
+
+		foreach ($npm as $a) {
+			$this->load->library('ciqrcode'); //pemanggilan library QR CODE
+ 
+	        $config['cacheable']    = true; //boolean, the default is true
+	        $config['cachedir']     = './assets/'; //string, the default is application/cache/
+	        $config['errorlog']     = './assets/'; //string, the default is application/logs/
+	        $config['imagedir']     = './assets/images/'; //direktori penyimpanan qr code
+	        $config['quality']      = true; //boolean, the default is true
+	        $config['size']         = '1024'; //interger, the default is 1024
+	        $config['black']        = array(224,255,255); // array, default is array(255,255,255)
+	        $config['white']        = array(70,130,180); // array, default is array(0,0,0)
+	        $this->ciqrcode->initialize($config);
+	 
+	        $image_name=$a->npm.'.png'; //buat name dari qr code sesuai dengan nim
+	 
+	        $params['data'] = $a->npm; //data yang akan di jadikan QR CODE
+	        $params['level'] = 'H'; //H=High
+	        $params['size'] = 10;
+	        $params['savename'] = FCPATH.$config['imagedir'].$image_name; //simpan image QR CODE ke folder assets/images/
+	        $this->ciqrcode->generate($params); // fungsi untuk generate QR CODE
+		}
+	}
+}
